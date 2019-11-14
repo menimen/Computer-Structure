bec367ad347f42c2eb6eeebb6a5c07f3acc1f41f
menimen <menash9288@gmail.com>
2019-11-14 19:37:48 +0200

    started working on ex1

---
+++ ex1 (1).c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "bytes.h"
+static int count = 0;
+byte_t *create_bytes(char *file_name)
+{
+
+}
+void print_bytes(byte_t *byte_array, FILE* out)
+{
+
+}
+void set_stud(byte_t *byte_array, int i, int j, int k)
+{
+	
+}
+float average_stud(byte_t *byte_array, int i)
+{
+	
+}
+float average_ans(byte_t *byte_array, int j)
+{
+	
+}
