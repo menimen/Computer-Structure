1341e4cfd91fe1fdd0d2dddefe8152c7fb4dda13
menimen <meni.ashurov@gmail.com>
2019-12-14 05:51:42 +0200

    backup

--- ex2/kefel.c
+++ ex2/kefel.c
@@ -1,5 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
+void deal_with_last_case_when_num_is_positive(FILE *file, int k) {
+  int cnt = 0,cntn=0,cntm=0,power_of_two = 1,started_hovering_flag=0,first_assign_to_registerebx=0, first_assign_to_registerecx=0;
+  int first_time_to_assign_to_eax,first_command_flag = 0,stand_on_first_bit_flag=0;
+  fprintf(file, "movl $0, %%eax\n");
+  fprintf(file, "movl %%edi, %%ebx\n");
+  while (power_of_two<k) {
+    if ((k & power_of_two) > 0 && started_hovering_flag == 0) {
+      started_hovering_flag = 1;
+      if (power_of_two != 1) {
+        cntm = cnt;
+        cntn = cntm;
+      }
+    } else if ((k & power_of_two) > 0 && started_hovering_flag == 1) {
+      cntn++;
+    } else {
+      started_hovering_flag = 0;
+      if (cntm == cntn) {
+        if((k&0X01) > 0 && stand_on_first_bit_flag == 0) {
+          if (first_time_to_assign_to_eax) {
+            first_time_to_assign_to_eax=1;
+            fprintf(file, "movl %%ebx, %%eax\n");
+          } else {
+            fprintf(file, "leal (%%eax,%%ebx), %%eax\n");
+          }
+          stand_on_first_bit_flag=1;
+        } else if(cntm!=0) {
+          fprintf(file, "sall $%d, %%ebx\n", cntm);
+          if (first_time_to_assign_to_eax) {
+            first_time_to_assign_to_eax=1;
+            fprintf(file, "movl %%ebx, %%eax\n");
+          } else {
+            fprintf(file, "leal (%%eax,%%ebx), %%eax\n");
+          }
+        }
+        started_hovering_flag = 0;
+      } else {
+        if(first_assign_to_registerecx == 0) {
+          first_assign_to_registerecx=1;
+          fprintf(file, "movl %%edi, %%ecx\n");
+        }
+        fprintf(file, "sall $%d, %%ebx\n", cntm);
+        fprintf(file, "sall $%d, %%ecx\n", cntn+1);
+        fprintf(file, "subl %%ebx,%%ecx\n");
+        fprintf(file, "leal (%%eax,%%ecx), %%eax\n");
+      }
+      cntn=0;
+      cntm=0;
+    }
+    power_of_two*=2;
+    cnt++;
+  }
+  if (started_hovering_flag == 1) {
+    if (cntm == cntn) {
+        fprintf(file, "sall $%d, %%ebx\n", cntm);
+        fprintf(file, "leal (%%eax,%%ebx), %%eax\n");
+    } else {
+      if(first_assign_to_registerecx == 0) {
+        first_assign_to_registerecx=1;
+        fprintf(file, "movl %%edi, %%ecx\n");
+      }
+      fprintf(file, "sall $%d, %%ebx\n", cntm);
+      fprintf(file, "sall $%d, %%ecx\n", cntn+1);
+      fprintf(file, "sub %%ebx,%%ecx\n");
+      fprintf(file, "leal (%%eax,%%ecx), %%eax\n");
+    }
+  }
+}
 int check_if_condition_three_possible(int k) {
   int power_of_two = 2;
   int cnt=0;
@@ -28,6 +95,7 @@ int check_if_condition_three_possible(int k) {
       return 0;
     }
   }
+  return 0;
 }
 int check_if_num_is_power_of_two(int k) {
   int cnt =0;
@@ -56,16 +124,16 @@ void deal_when_num_is_power_of_two(FILE* file, int k) {
   }
   power_of_two /=2;
   k = k / power_of_two;
-  fprintf(file, "sall $%d,%%eax\n",cnt);
+  fprintf(file, "kefel:  sall $%d,%%eax\n",cnt);
 }
 void deal_with_three(FILE* file, int k) {
-  fprintf(file, "leal (%%edi,%%edi,2), %%eax\n");
+  fprintf(file, "kefel: leal (%%edi,%%edi,2), %%eax\n");
 }
 void deal_with_five(FILE* file, int k) {
-  fprintf(file, "leal (%%edi,%%edi,4), %%eax\n");
+  fprintf(file, "kefel: leal (%%edi,%%edi,4), %%eax\n");
 }
 void deal_with_nine(FILE* file, int k) {
-  fprintf(file, "leal (%%edi,%%edi,8), %%eax\n");
+  fprintf(file, "kefel:  leal (%%edi,%%edi,8), %%eax\n");
 }
 int fill_number_with_power_of_two(FILE* file, int k) {
   int power_of_two = 2;
@@ -80,32 +148,28 @@ int fill_number_with_power_of_two(FILE* file, int k) {
   return k;
 }
 int main(int argc, char *argv[]) {
-  int neg_flag = 0;
+  int neg_flag = 0, first_command_flag = 0;
   int j = 0;
   int k = atoi(argv[1]);
   FILE* file = fopen("kefel.s","w");
   fprintf(file, ".section .text\n");
   fprintf(file, ".globl  kefel\n");
-  fprintf(file, "kefel:\n");
   if (k==0) {
-    fprintf(file, "movl $0, %%eax\n");
+    fprintf(file, "kefel :  movl $0, %%eax\n");
     fprintf(file, "ret\n");
     fclose(file);
     return 0;
   } else if (k==1) {
+    fprintf(file, "kefel:\n");
     fprintf(file, "ret\n");
     fclose(file);
     return 0;
   } else if(k==-1){
-    fprintf(file, "movl %%edi, %%eax\n");
+    fprintf(file, "kefel:  movl %%edi, %%eax\n");
     fprintf(file, "neg %%eax\n");
     fprintf(file, "ret\n");
     fclose(file);
     return 0;
-  }
-  if (k < 0) {
-    k = k * -1;
-    neg_flag = 1;
   }
   if(k==9){
     deal_with_nine(file,k);
@@ -127,17 +191,14 @@ int main(int argc, char *argv[]) {
         k = k/3;
         k = fill_number_with_power_of_two(file, k);
       }
-      if (k % 9 == 0) {
-        deal_with_nine(file, k);
-      } else if (k % 5 == 0) {
-        deal_with_five(file, k);
-      } else if (k % 3 == 0) {
-        deal_with_three(file, k);
-      }
     }else if(check_if_num_is_power_of_two(k) == 1) {
     deal_when_num_is_power_of_two(file,k);
   } else{
-
+    fprintf(file, "kefel:\n");
+    if(neg_flag == 1) {
+    } else {
+      deal_with_last_case_when_num_is_positive(file,k);
+    }
   }
   if (neg_flag == 1) {
     fprintf(file, "neg %%eax\n");
